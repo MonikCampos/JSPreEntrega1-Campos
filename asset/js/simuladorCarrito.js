ca817d3445156ca52fd06f4afb5e388d4f52241f
monicampos <monica.alejandra.campos@gmail.com>
2023-04-20 23:51:46 -0300

    Segunda version simulador JS

--- asset/js/simuladorCarrito.js
+++ asset/js/simuladorCarrito.js
@@ -2,7 +2,11 @@ let carrito = [];
 let total = 0;
 let cuotas = 1;
 let interes = 0;
+const iva = 21;
 let totalSinInteres = 0;
+let totalConIva = 0;
+let totalConIntereses = 0 ;
+let cadaCuota = 0;
 
 function seleccionarProducto(nombre, precio) {
     let producto = {
@@ -14,31 +18,43 @@ function seleccionarProducto(nombre, precio) {
     };
     carrito.push(producto);
     total += precio;
+    calcularIva(total,iva);
     mostrarResumen();
 }
 
+function calcularIva(totalSinIva, tasaIva) {
+    let porcentajeIva = totalSinIva * (tasaIva / 100);
+    totalConIva = total + porcentajeIva;
+    totalSinInteres = totalConIva
+    return { totalConIva };
+}
+
+
 function aplicarCuotas() {
-    totalSinInteres = total;
+    totalSinInteres = totalConIva;
     cuotas = prompt("Ingrese la cantidad de cuotas:");
     interes = prompt("Ingrese el interés (%):");
-    let intereses = total * (interes / 100);
-    total += intereses;
+    let intereses = totalConIva * (interes / 100);
+    totalConIntereses = totalConIva + intereses;
     for (let i = 0; i < carrito.length; i++) {
         let producto = carrito[i];
         producto.cuotas = cuotas;
         producto.interes = interes;
-        let porcentajeDelTotal = producto.subtotal / total;
+        let porcentajeDelTotal = producto.subtotal / totalConIva;
         producto.subtotal = producto.precio + (porcentajeDelTotal * intereses);
     }
+    cadaCuota = totalConIntereses / cuotas;
     mostrarResumen();
 }
 
-function limpiarResumen() {
+function limpiarCompra() {
     carrito = [];
     total = 0;
     cuotas = 1;
     interes = 0;
     totalSinInteres = 0;
+    totalConIva = 0;
+    cadaCuota = 0;
     mostrarResumen();
 }
 
@@ -53,7 +69,10 @@ function mostrarResumen() {
         listaResumen.appendChild(li);
     }
     document.getElementById("totalSinInteres").innerHTML = totalSinInteres;
+    document.getElementById("totalConIntereses").innerHTML = totalConIntereses;
+    document.getElementById("totalConIva").innerHTML = totalConIva;
     document.getElementById("total").innerHTML = total.toFixed(2);
     document.getElementById("cuotas").innerHTML = cuotas;
+    document.getElementById("cadaCuota").innerHTML = cadaCuota;
     document.getElementById("interes").innerHTML = interes;
 }
